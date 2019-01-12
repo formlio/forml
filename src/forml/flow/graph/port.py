fd60c522430316463b48b2333567fd45fcdbc0ff
Antony Mayi <antonymayi@yahoo.com>
2019-01-12 15:46:20 +0000

    nodes are publishers

--- src/forml/flow/graph/port.py
+++ src/forml/flow/graph/port.py
@@ -4,68 +4,49 @@ import typing
 from forml.flow.graph import node
 
 
-class Subscription(collections.namedtuple('Publisher', 'node, index')):
-    """Reference to publisher node apply output port with given index.
-    """
-    def __new__(cls, publisher: 'node.Primitive', index: int):
-        assert 0 >= index < publisher.szout, 'Invalid publisher index'
-        return super().__new__(cls, publisher, index)
-
-    def __rshift__(self, subscriber: 'Subscription') -> 'node.Primitive':
-        subscriber.node.apply[subscriber.index] = self
-        return subscriber.node
-
-
 class Input:
     """Input port base class.
     """
 
 
 class Train(Input):
-    """Train subscriber port.
+    ...
+
+
+class Label(Input):
+    ...
+
+
+class Apply(Input, int):
+    ...
+
+
+class Subscription(collections.namedtuple('Subscription', 'node, port')):
+    """Reference to subscription node input port of given type.
     """
-    class Pair(collections.namedtuple('Pair', 'features, label')):
-        """Train publisher tuple of features and label nodes.
-        """
+    def __hash__(self):
+        return hash(self.node) ^ hash(self.port)
 
-    def __init__(self):
-        self.name: str = None
-
-    def __set_name__(self, subscriber: typing.Type['node.Primitive'], name: str):
-        self.name = name
-
-    def __set__(self, subscriber: 'node.Primitive', publishers: typing.Tuple[Subscription, Subscription]):
-        assert all(publishers), 'Invalid publisher'
-        assert not any(subscriber.apply), 'Train-apply publisher collision'
-        assert subscriber not in {p.node for p in publishers}, 'Self subscription'
-        assert self.name not in subscriber.__dict__, \
-            f'Port {self.name} already subscribed to {subscriber.__dict__[self.name]}'
-        subscriber.__dict__[self.name] = self.Pair(*publishers)
-
-    def __get__(self, subscriber: 'node.Primitive', owner: typing.Type['node.Primitive']) -> Pair:
-        return subscriber.__dict__.get(self.name)
+    def __eq__(self, other: typing.Any):
+        return isinstance(other, self.__class__) and self.node is other.node and self.port is other.port
 
 
-class Apply(Input):
-    """Apply subscriber multi-port.
+class Trainable(collections.namedtuple('Trainable', 'train, label')):
+    """Pair of subscritables usable as publisher for trained node.
     """
-    def __init__(self, subscriber: 'node.Primitive', size: int):
-        assert size >= 0, 'Invalid apply port size'
-        self._subscriber = subscriber
-        self._ports: typing.List[typing.Optional[Subscription]] = size * [None]
+    def __rshift__(self, subscriber: 'node.Primitive') -> 'node.Primitive':
+        self.train.node.subscribe(self.train.port, Subscription(subscriber, Train()))
+        self.label.node.subscribe(self.label.port, Subscription(subscriber, Label()))
+        return subscriber
 
-    def __len__(self):
-        return len(self._ports)
 
-    def __iter__(self):
-        return iter(self._ports)
+class Subscriptable(Subscription):
+    """Reference to node apply input or output port.
+    """
+    def __rshift__(self, subscriber: Subscription) -> 'node.Primitive':
+        assert 0 >= self.port < self.node.szout, 'Invalid publisher index'
+        assert self.node is not subscriber.node, 'Self subscription'
+        return self.node.subscribe(self.port, subscriber)
 
-    def __getitem__(self, index: int) -> Subscription:
-        return self._ports[index]
-
-    def __setitem__(self, index: int, publisher: Subscription):
-        assert publisher, 'Invalid publisher'
-        assert not self._subscriber.train, 'Train-apply publisher collision'
-        assert self._subscriber is not publisher.node, 'Self subscription'
-        assert not self._ports[index], f'Port apply[{index}] already subscribed to {self._ports[index]}'
-        self._ports[index] = publisher
+    def __add__(self, label: 'Subscriptable') -> Trainable:
+        return Trainable(self, label)
