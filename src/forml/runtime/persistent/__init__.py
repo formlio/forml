cc5c626a7ef901f71827f6608053d51002c4ffef
Antony Mayi <antonymayi@yahoo.com>
2019-03-30 17:43:17 +0000

    save

--- src/forml/runtime/persistent/__init__.py
+++ src/forml/runtime/persistent/__init__.py
@@ -1,5 +1,7 @@
+"""
+Runtime persistence.
+"""
 import abc
-import collections
 import logging
 import typing
 import uuid
@@ -53,125 +55,179 @@ class Lineage:
 class Registry(metaclass=abc.ABCMeta):
     """must be serializable.
     """
-    STEP = 1
+    class Listing:
+        class Empty(runtime.Error):
+            """Exception indicating empty listing.
+            """
 
-    class Empty(runtime.Error):
-        """
-        """
+        STEP = 1
+
+        def __init__(self, items: typing.Iterable[int], step: int = STEP):
+            self._items: typing.Iterable[int] = items
+            self._step = step
+
+        @property
+        def last(self) -> int:
+            try:
+                return max(self._items)
+            except ValueError:
+                raise self.Empty('Empty listing')
+
+        @property
+        def next(self) -> int:
+            try:
+                return self.last + self._step
+            except self.Empty:
+                return self._step
 
     def get(self, project: str, lineage: typing.Optional[int] = None) -> Lineage:
         def getter(generation: typing.Optional[int]) -> Generation:
-            def reader(sid: uuid.UUID) -> bytes:
-                if sid not in record.states:
-                    raise KeyError('Unknown key: %s', sid)
-                return self._read(sid)
-
             if not generation:
-                generation = self._last(self._generations(project, lineage))
+                generation = self._generations(project, lineage).last
             record = self._open(project, lineage, generation)
             return Generation(record, reader)
 
-        def writer(state: bytes) -> uuid.UUID:
-            sid = uuid.uuid4()
-            self._write(sid, state)
-            return sid
-        
         def closer(record: resource.Record) -> int:
-            generation = self._next(self._generations(project, lineage))
+            generation = self._generations(project, lineage).next
             self._close(project, lineage, generation, record)
             return generation
 
         if not lineage:
-            lineage = self._last(self._lineages(project))
+            lineage = self._lineages(project).last
 
         return Lineage(self._pull(project, lineage), getter, writer, closer)
 
     def put(self, project: str, artifact: prjmod.Artifact) -> Lineage:
-        lineage = self._next(self._lineages(project))
+        lineage = self._lineages(project).next
         self._push(project, lineage, artifact)
         return self.get(project, lineage)
 
-    @classmethod
-    def _last(cls, listing: typing.Iterable[int]) -> int:
-        try:
-            return max(listing)
-        except ValueError:
-            raise cls.Empty('Empty listing')
+    def dump(self, state: bytes) -> uuid.UUID:
+        """Dump an unbound state (not belonging to any project) under given state id.
 
-    @classmethod
-    def _next(cls, listing: typing.Iterable[int]) -> int:
-        try:
-            return cls._last(listing) + cls.STEP
-        except cls.Empty:
-            return cls.STEP
+        An unbound state is expected to be committed later into a new generation of specific lineage.
 
-    @abc.abstractmethod
-    def _lineages(self, project: str) -> typing.Sequence[int]:
-        """
-        Returns:
-        """
-
-    @abc.abstractmethod
-    def _generations(self, project: str, lineage: int) -> typing.Sequence[int]:
-        """
         Args:
-            project:
-            lineage:
+            state: Serialized state to be persisted.
 
-        Returns:
+        Returns: Associated state id.
+        """
+        sid = uuid.uuid4()
+        LOGGER.debug('%s: Dumping state %s', self, sid)
+        self._write(sid, state)
+        return sid
 
+    def load(self, sid: uuid.UUID) -> bytes:
+        """Load the state based on provided id.
+
+        Args:
+            sid: Id of the state object to be loaded.
+
+        Returns: Serialized state.
+        """
+        LOGGER.debug('%s: Loading state %s', self, sid)
+        return self._read(sid)
+
+    def __str__(self):
+        return f'{self.__class__.__name__}'
+
+    @abc.abstractmethod
+    def _lineages(self, project: str) -> 'Registry.Listing':
+        """List the lineages of given project.
+
+        Args:
+            project: Project to be listed.
+
+        Returns: Lineages listing.
+        """
+
+    @abc.abstractmethod
+    def _generations(self, project: str, lineage: int) -> 'Registry.Listing':
+        """List the generations of given lineage.
+
+        Args:
+            project: Project of which the lineage is to be listed.
+            lineage: Lineage of the project to be listed.
+
+        Returns: Generations listing.
         """
 
     @abc.abstractmethod
     def _pull(self, project: str, lineage: int) -> prjmod.Artifact:
-        ...
+        """Return the artifact of given lineage.
+
+        Args:
+            project: Project of which the lineage artifact is to be returned.
+            lineage: Lineage of the project to return the artifact of.
+
+        Returns: Project artifact object.
+        """
 
     @abc.abstractmethod
     def _push(self, project: str, lineage: int, artifact: prjmod.Artifact) -> None:
-        """
+        """Start new lineage of a project based on given artifact.
 
         Args:
-            project:
-            artifact:
-
-        Returns: New lineage id.
+            project: Project to start the new lineage in.
+            artifact: Artifact of the new lineage.
         """
 
     @abc.abstractmethod
     def _read(self, sid: uuid.UUID) -> bytes:
-        ...
+        """Load the state based on provided id.
+
+        Args:
+            sid: Id of the state object to be loaded.
+
+        Returns: Serialized state.
+        """
 
     @abc.abstractmethod
     def _write(self, sid: uuid.UUID, state: bytes) -> None:
-        """
-
-        Find first not sealed generation with given component missing or create new empty one.
+        """Dump an unbound state under given state id.
 
         Args:
-            project:
-            lineage:
-            component:
-            state:
-
-        Returns: New id.
-        """
-
-    @abc.abstractmethod
-    def _close(self, project: str, lineage: int, generation: int, meta: resource.Record) -> None:
-        """
-        Args:
-            project:
-            lineage:
-            component:
-            state:
-
-        Returns: New generation id.
+            sid: state id to associate the payload with.
+            state: Serialized state to be persisted.
         """
 
     def _open(self, project: str, lineage: int, generation: int) -> resource.Record:
-        ...
+        """Return the metadata record of given generation.
+
+        Args:
+            project:
+            lineage:
+            generation:
+
+        Returns: Generation metadata.
+        """
+
+    @abc.abstractmethod
+    def _close(self, project: str, lineage: int, generation: int, record: resource.Record) -> None:
+        """Seal new generation by storing its metadata record.
+
+        Args:
+            project: Project to store the metadata into.
+            lineage: Lineage of the project to store the metadata into.
+            generation: Generation of the project to store the metadata into.
+            record: Generation metadata to be stored.
+        """
 
 
 class Assets:
     """Persistent assets IO for loading and dumping models.
     """
+    def __init__(self, registry: Registry, project: str, lineage: typing.Optional[int] = None,
+                 generation: typing.Optional[int] = None):
+        self._registry: Registry = registry
+        self._lineage: Lineage = registry.get(project, lineage)
+        self._generation: typing.Optional[int]
+
+    def load(self, sid: uuid.UUID) -> bytes:
+        return self._registry.load(sid)
+
+    def dump(self, state: bytes) -> uuid.UUID:
+        return self._registry.dump(state)
+
+    def commit(self, record: resource.Record) -> None:
+        self._lineage.put(record)
