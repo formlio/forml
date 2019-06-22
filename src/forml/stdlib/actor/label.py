6a6395c9dad1cc0c48a6fb400abc1186c7f2ccf9
Antony Mayi <antonymayi@yahoo.com>
2019-06-22 14:59:13 +0000

    Merged PR 609: improved debug operators

--- src/forml/stdlib/actor/label.py
+++ src/forml/stdlib/actor/label.py
@@ -1,12 +1,15 @@
 """
-Label extraction actors.
+Label manipulation actors.
 """
-
+import logging
 import typing
 
 import pandas
 
 from forml.flow import task
+from forml.stdlib.actor import frame
+
+LOGGER = logging.getLogger(__name__)
 
 
 class ColumnExtractor(task.Actor):
@@ -15,6 +18,7 @@ class ColumnExtractor(task.Actor):
     def __init__(self, column: str = 'label'):
         self.column: str = column
 
+    @frame.ndframed
     def apply(self, features: pandas.DataFrame) -> typing.Tuple[  # pylint: disable=arguments-differ
             pandas.DataFrame, pandas.Series]:
         """Transforming the input feature set into two outputs separating the label column into the second one.
@@ -25,6 +29,50 @@ class ColumnExtractor(task.Actor):
         Returns: Features with label column removed plus just the label column in second new dataset.
         """
         return features.drop(columns=self.column), features[self.column]
+
+    def get_params(self) -> typing.Dict[str, typing.Any]:
+        """Standard param getter.
+
+        Returns: Actor params.
+        """
+        return {'column': self.column}
+
+    def set_params(self, column: str) -> None:  # pylint: disable=arguments-differ
+        """Standard params setter.
+
+        Args:
+            column: Label column name.
+        """
+        self.column = column
+
+
+class ColumnInserter(task.Actor):
+    """Label-extraction inversion - inserting a label as a new column to the feature set.
+    """
+    def __init__(self, column: str = 'label'):
+        self.column: str = column
+        self._label: typing.Optional[pandas.Series] = None
+
+    def train(self, features: pandas.DataFrame, label: pandas.Series) -> None:
+        """Train the inserter by remembering the labels.
+        Args:
+            features: X table.
+            label: Y series.
+        """
+        self._label = label
+
+    @frame.ndframed
+    def apply(self, features: pandas.DataFrame) -> pandas.DataFrame:  # pylint: disable=arguments-differ
+        """Transforming the input feature set into two outputs separating the label column into the second one.
+
+        Args:
+            features: Input data set.
+
+        Returns: Features with label column removed plus just the label column in second new dataset.
+        """
+        if self._label is None or len(features) != len(self._label):
+            raise RuntimeError('Inserter not trained')
+        return features.set_index(self._label.rename(self.column)).reset_index()
 
     def get_params(self) -> typing.Dict[str, typing.Any]:
         """Standard param getter.
--- src/forml/stdlib/operator/debug.py
+++ src/forml/stdlib/operator/debug.py
@@ -4,35 +4,72 @@ Debugging operators.
 
 import os
 import secrets
-import time
 
-import pandas
+from forml.flow import task, pipeline
+from forml.flow.graph import node, view
+from forml.flow.pipeline import topology
+from forml.stdlib.actor import label as labelmod, frame
 
-from forml.flow import task
-from forml.stdlib.operator import simple
 
-
-@simple.Mapper.operator
-class Dump(task.Actor):
-    """Pass-through transformer that dumps the input datasets to CSV files.
+class Return(topology.Operator):
+    """Transformer that for train flow re-inserts label back to the frame and returns it (apply flow remains unchanged).
+    This is useful for cutting a pipeline and appending this operator to return the dataset as is for debugging.
     """
-    SEQ_RESOLUTION = 604800
+    def __init__(self, label: str = 'label'):
+        self.inserter: task.Spec = labelmod.ColumnInserter.spec(column=label)
+
+    def compose(self, left: topology.Composable) -> pipeline.Segment:
+        """Composition implementation.
+
+        Args:
+            left: Left side.
+
+        Returns: Composed track.
+        """
+        left: pipeline.Segment = left.expand()
+        inserter_apply: node.Worker = node.Worker(self.inserter, 1, 1)
+        inserter_train: node.Worker = inserter_apply.fork()
+        inserter_train.train(left.train.publisher, left.label.publisher)
+        return left.extend(train=view.Path(inserter_apply))
+
+
+class Dump(topology.Operator):
+    """Transparent transformer that dumps the input datasets to CSV files.
+    """
     CSV_SUFFIX = '.csv'
 
-    def __init__(self, path: str = ''):
+    def __init__(self, path: str = '', label: str = 'label'):
         self.dir: str = os.path.dirname(path)
         name, suffix = os.path.splitext(os.path.basename(path))
         self.name: str = name or secrets.token_urlsafe(8)
         self.suffix: str = suffix or self.CSV_SUFFIX
+        self.inserter: task.Spec = labelmod.ColumnInserter.spec(column=label)
+        self._instances: int = 0
 
-    def apply(self, features: pandas.DataFrame) -> pandas.DataFrame:  # pylint: disable=arguments-differ
-        """Transformer logic.
+    def _path(self, mode: str) -> str:
+        """Generate the target path.
 
         Args:
-            *features: Input frames.
+            mode: Pipeline operation mode.
 
-        Returns: Original unchanged frames.
+        Returns: Path value.
         """
-        seq = int(time.time() % self.SEQ_RESOLUTION)
-        features.to_csv(os.path.join(self.dir, f'{self.name}-{seq}{self.suffix}'), index=False)
-        return features
+        return os.path.join(self.dir, f'{self.name}-{mode}-{self._instances}{self.suffix}')
+
+    def compose(self, left: topology.Composable) -> pipeline.Segment:
+        """Composition implementation.
+
+        Args:
+            left: Left side.
+
+        Returns: Composed track.
+        """
+        left: pipeline.Segment = left.expand()
+        inserter_apply: node.Worker = node.Worker(self.inserter, 1, 1)
+        inserter_train: node.Worker = inserter_apply.fork()
+        inserter_train.train(left.train.publisher, left.label.publisher)
+        dumper_train: node.Worker = node.Worker(frame.Dump.spec(path=self._path('train')), 1, 1)
+        dumper_apply: node.Worker = node.Worker(frame.Dump.spec(path=self._path('apply')), 1, 1)
+        dumper_train[0].subscribe(inserter_apply[0])
+        self._instances += 1
+        return left.extend(apply=view.Path(dumper_apply), train=view.Path(inserter_apply, dumper_train))
