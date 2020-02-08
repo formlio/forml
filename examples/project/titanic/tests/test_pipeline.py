2fdc566f1b09734f8c1e9bf3a197b9bd9b8f140b
Antony Mayi <antonymayi@yahoo.com>
2020-02-08 13:56:32 +0000

    save

--- examples/project/titanic/tests/test_pipeline.py
+++ examples/project/titanic/tests/test_pipeline.py
@@ -1,37 +1,8 @@
 from forml import testing
+from forml.testing import spec
 from titanic.pipeline import preprocessing
-#
-#
-# with testing.Actor(preprocessing.NaNImputer.spec(1, 10)) as actor:
-#     actor(*in_).raises(exception)
-#     actor(*in_).returns(*out_)
-#
-#
 
 
-# testing operator
-# * separate test for apply, train, label
-
-with testing.Operator(preprocessing.NaNImputer(1, 2, 3)) as operator:
-    with operator(apply, train, label) as undertest:
-        undertest.apply.equals(asd)
-        undertest.train.raises(dsa)
-        undertest.label == qwe
-
-    operator.apply(*in_).returns(*out_)
-    operator.train(*in_).raises(exception)
-    operator.label(*in_).returns(None)
-
-
-class TestNanImputer(testing.Operator(preprocessing.NaNImputer(1, 2, 3))):
-
-    @testing.input(apply, train, label)
-    def test_invalid(self):
-        self.operator.apply(*in_).returns(*out_)
-        operator.train(*in_).raises(exception)
-        operator.label(*in_).returns(None)
-
-
-
-hyperparams
-train_in, label_in, apply_in -> train_out, label_out, apply_out
+class MyTest(testing.operator(preprocessing.NaNImputer)):
+    invalid_params = spec.Case(foo=1).raises(ValueError)
+    not_trained = spec.Case(foo=1).apply('foo').raises(ValueError)
--- tests/testing/test_spec.py
+++ tests/testing/test_spec.py
@@ -3,8 +3,9 @@ ForML testing unit tests.
 """
 # pylint: disable=protected-access,no-self-use
 
-import pytest
 import typing
+
+import pytest
 
 from forml.testing import spec
 
@@ -14,36 +15,175 @@ class TestCase:
     """
     @staticmethod
     @pytest.fixture(scope='session')
-    def case(hyperparams: typing.Mapping[str, int]) -> spec.Case:
-        """Case fixture.
+    def exception() -> typing.Type[Exception]:
+        """Exception fixture.
+
+        Returns: Exception type.
         """
-        return spec.Case(**hyperparams)
+        return AssertionError
 
     @staticmethod
     @pytest.fixture(scope='session')
-    def params(hyperparams: typing.Mapping[str, int]) -> spec.Scenario.Params:
-        """Params fixture.
-        """
-        return spec.Scenario.Params(**hyperparams)
+    def apply_input() -> str:
+        """IO fixture.
 
-    def test_raises(self, case: spec.Case, params: spec.Scenario.Params):
-        """Raises assertion test.
+        Returns: String value.
         """
-        exception = RuntimeError
-        assert case.raises(exception) == spec.Scenario(params, exception=exception)
-        assert case.apply('foo').raises(exception) == spec.Scenario(
-            params, spec.Scenario.Data(apply='foo'), exception=exception)
-        assert case.train('foo', 'bar').raises(exception) == spec.Scenario(
-            params, spec.Scenario.Data(train='foo', label='bar'), exception=exception)
-        assert case.train('foo', 'bar').apply('baz').raises(exception) == spec.Scenario(
-            params, spec.Scenario.Data(apply='baz', train='foo', label='bar'), exception=exception)
+        return 'foo'
 
-    def test_returns(self, case: spec.Case, params: spec.Scenario.Params):
-        """Returns assertion test.
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def apply_output() -> str:
+        """IO fixture.
+
+        Returns: String value.
         """
-        assert case.apply('foo').returns('FOO') == spec.Scenario(
-            params, spec.Scenario.Data(apply='foo'), spec.Scenario.Data(apply='FOO'))
-        assert case.train('foo', 'bar').returns('FOO', 'BAR') == spec.Scenario(
-            params, spec.Scenario.Data(train='foo', label='bar'), spec.Scenario.Data(train='FOO', label='BAR'))
-        assert case.train('foo', 'bar').apply('baz').returns('BAZ') == spec.Scenario(
-            params, spec.Scenario.Data(apply='baz', train='foo', label='bar'), spec.Scenario.Data(apply='BAZ'))
+        return 'bar'
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def train_input() -> str:
+        """IO fixture.
+
+        Returns: String value.
+        """
+        return 'foo'
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def label_input() -> str:
+        """IO fixture.
+
+        Returns: String value.
+        """
+        return 'bar'
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def train_output() -> str:
+        """IO fixture.
+
+        Returns: String value.
+        """
+        return 'baz'
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def init_raises(hyperparams: typing.Mapping[str, int], exception: typing.Type[Exception]) -> spec.Scenario:
+        """Scenario fixture.
+        """
+        return spec.Case(**hyperparams).raises(exception)
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def plainapply_raises(hyperparams: typing.Mapping[str, int], apply_input: str,
+                          exception: typing.Type[Exception]) -> spec.Scenario:
+        """Scenario fixture.
+        """
+        return spec.Case(**hyperparams).apply(apply_input).raises(exception)
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def plainapply_returns(hyperparams: typing.Mapping[str, int], apply_input: str, apply_output: str) -> spec.Scenario:
+        """Scenario fixture.
+        """
+        return spec.Case(**hyperparams).apply(apply_input).returns(apply_output)
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def statetrain_raises(hyperparams: typing.Mapping[str, int], train_input: str, label_input: str,
+                          exception: typing.Type[Exception]) -> spec.Scenario:
+        """Scenario fixture.
+        """
+        return spec.Case(**hyperparams).train(train_input, label_input).raises(exception)
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def statetrain_returns(hyperparams: typing.Mapping[str, int], train_input: str, label_input: str,
+                           train_output: str) -> spec.Scenario:
+        """Scenario fixture.
+        """
+        return spec.Case(**hyperparams).train(train_input, label_input).returns(train_output)
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def stateapply_raises(hyperparams: typing.Mapping[str, int], train_input: str, label_input: str,
+                          apply_input, exception: typing.Type[Exception]) -> spec.Scenario:
+        """Scenario fixture.
+        """
+        return spec.Case(**hyperparams).train(train_input, label_input).apply(apply_input).raises(exception)
+
+    @staticmethod
+    @pytest.fixture(scope='session')
+    def stateapply_returns(hyperparams: typing.Mapping[str, int], train_input: str, label_input: str,
+                           apply_input: str, apply_output: str) -> spec.Scenario:
+        """Scenario fixture.
+        """
+        return spec.Case(**hyperparams).train(train_input, label_input).apply(apply_input).returns(apply_output)
+
+    def test_init_raises(self, init_raises: spec.Scenario, hyperparams: typing.Mapping[str, int], exception: Exception):
+        """Scenario spec test case.
+        """
+        assert init_raises.outcome == spec.Scenario.Outcome.INIT_RAISES
+        assert init_raises.params.kwargs == hyperparams
+        assert init_raises.exception == exception
+
+    def test_plainapply_raises(self, plainapply_raises: spec.Scenario, hyperparams: typing.Mapping[str, int],
+                               apply_input: str, exception: Exception):
+        """Scenario spec test case.
+        """
+        assert plainapply_raises.outcome == spec.Scenario.Outcome.PLAINAPPLY_RAISES
+        assert plainapply_raises.params.kwargs == hyperparams
+        assert plainapply_raises.input.apply == apply_input
+        assert plainapply_raises.exception == exception
+
+    def test_plainapply_returns(self, plainapply_returns: spec.Scenario, hyperparams: typing.Mapping[str, int],
+                                apply_input: str, apply_output: str):
+        """Scenario spec test case.
+        """
+        assert plainapply_returns.outcome == spec.Scenario.Outcome.PLAINAPPLY_RETURNS
+        assert plainapply_returns.params.kwargs == hyperparams
+        assert plainapply_returns.input.apply == apply_input
+        assert plainapply_returns.output.apply == apply_output
+
+    def test_statetrain_raises(self, statetrain_raises: spec.Scenario, hyperparams: typing.Mapping[str, int],
+                               train_input: str, label_input: str, exception: Exception):
+        """Scenario spec test case.
+        """
+        assert statetrain_raises.outcome == spec.Scenario.Outcome.STATETRAIN_RAISES
+        assert statetrain_raises.params.kwargs == hyperparams
+        assert statetrain_raises.input.train == train_input
+        assert statetrain_raises.input.label == label_input
+        assert statetrain_raises.exception == exception
+
+    def test_statetrain_returns(self, statetrain_returns: spec.Scenario, hyperparams: typing.Mapping[str, int],
+                                train_input: str, label_input: str, train_output: str):
+        """Scenario spec test case.
+        """
+        assert statetrain_returns.outcome == spec.Scenario.Outcome.STATETRAIN_RETURNS
+        assert statetrain_returns.params.kwargs == hyperparams
+        assert statetrain_returns.input.train == train_input
+        assert statetrain_returns.input.label == label_input
+        assert statetrain_returns.output.train == train_output
+
+    def test_stateapply_raises(self, stateapply_raises: spec.Scenario, hyperparams: typing.Mapping[str, int],
+                               train_input: str, label_input: str, apply_input: str, exception: Exception):
+        """Scenario spec test case.
+        """
+        assert stateapply_raises.outcome == spec.Scenario.Outcome.STATEAPPLY_RAISES
+        assert stateapply_raises.params.kwargs == hyperparams
+        assert stateapply_raises.input.train == train_input
+        assert stateapply_raises.input.label == label_input
+        assert stateapply_raises.input.apply == apply_input
+        assert stateapply_raises.exception == exception
+
+    def test_stateapply_returns(self, stateapply_returns: spec.Scenario, hyperparams: typing.Mapping[str, int],
+                                train_input: str, label_input: str, apply_input: str, apply_output: str):
+        """Scenario spec test case.
+        """
+        assert stateapply_returns.outcome == spec.Scenario.Outcome.STATEAPPLY_RETURNS
+        assert stateapply_returns.params.kwargs == hyperparams
+        assert stateapply_returns.input.train == train_input
+        assert stateapply_returns.input.label == label_input
+        assert stateapply_returns.input.apply == apply_input
+        assert stateapply_returns.output.apply == apply_output
