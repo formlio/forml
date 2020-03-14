1f9e57b124cddb68c2a9504a5ca507a954335da5
Antony Mayi <antonymayi@yahoo.com>
2020-03-14 21:47:37 +0000

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -63,4 +63,6 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_show_sourcelink = False
+
 html_show_copyright = False
