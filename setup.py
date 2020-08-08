2c87d04d1367a32275bd0da616bc4eeb31076428
Antony Mayi <antonymayi@yahoo.com>
2020-08-08 14:42:17 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# 'License'); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 """
 Setuptools script for ForML package.
 """
@@ -40,8 +58,8 @@ setuptools.setup(name='forml',
                  long_description=open('README.md', 'r').read(),
                  long_description_content_type='text/markdown',
                  url='https://github.com/formlio/forml',
-                 author='ForML Authors',
-                 author_email='noreply@forml.io',
+                 maintainer='ForML Development Team',
+                 maintainer_email='noreply@forml.io',
                  license='Apache License 2.0',
                  packages=setuptools.find_packages(where='src'),
                  package_dir={'': 'src'},
@@ -66,7 +84,7 @@ setuptools.setup(name='forml',
                      'Environment :: Console',
                      'Intended Audience :: Developers',
                      'Intended Audience :: Science/Research',
-                     'License :: Other/Proprietary License',
+                     'License :: OSI Approved :: Apache Software License',
                      'Programming Language :: Python :: 3',
                      'Topic :: Scientific/Engineering :: Artificial Intelligence',
                      'Topic :: System :: Distributed Computing'
