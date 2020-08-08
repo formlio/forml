cbb470a2c5533f5346bcf41c783222fce684da00
Antony Mayi <antonymayi@yahoo.com>
2020-08-08 13:23:57 +0000

    updating pre-commit

--- src/forml/io/dsl/schema/series.py
+++ src/forml/io/dsl/schema/series.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 """
 ETL schema types.
 """
@@ -150,14 +167,23 @@ class Column(tuple, metaclass=abc.ABCMeta):
         """
 
     @classmethod
-    def disect(cls, column: 'Column') -> typing.AbstractSet['Column']:
-        """Return an iterable of instances of this type composing given column.
+    def disect(cls, *column: 'Column') -> typing.AbstractSet['Column']:
+        """Return an iterable of instances of this type composing given column(s).
 
-        Returns: Set of this type instances used in given column.
+        Returns: Set of this type instances used in given column(s).
         """
-        disector = cls.Disect(cls)
-        column.accept(disector)
-        return disector.terms
+        def visit(subject: 'Column') -> typing.Iterable['Column']:
+            """Disect a single column.
+
+            Args:
+                subject: Column to be disected.
+
+            Returns: Disected column instances.
+            """
+            disector = cls.Disect(cls)
+            subject.accept(disector)
+            return disector.terms
+        return {t for c in column for t in visit(c)}
 
     @classmethod
     def ensure(cls, column: 'Column') -> 'Column':
