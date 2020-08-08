29b731437bc9ad0e2913cc80fd356bb6477aaaaf
Antony Mayi <antonymayi@yahoo.com>
2020-08-08 15:57:30 +0000

    adding license

--- src/forml/io/feed/sql.py
+++ src/forml/io/feed/sql.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 """
 ANSI SQL ETL feed.
 """
@@ -87,7 +104,7 @@ class Feed(io.Feed):
                 function.Or: Expression('{} OR {}'),
                 function.Not: Expression('NOT {}'),
                 function.Cast: Expression('cast({} AS {})', lambda _, k: [_, Feed.Reader.Parser.KIND[k]]),
-                function.Count: Expression('count({})', lambda c=None: [c if c is not None else "*"])
+                function.Count: Expression('count({})', lambda c=None: [c if c is not None else '*'])
             }
 
             JOIN: typing.Mapping[stmtmod.Join.Kind, str] = {
