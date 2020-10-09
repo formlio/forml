0128304fc2a8b315bc42af5d3371138a5075a5fb
Antony Mayi <antonymayi@yahoo.com>
2020-10-09 18:42:55 +0000

    conf refactoring

--- src/forml/conf/provider.py
+++ src/forml/conf/provider.py
@@ -18,82 +18,55 @@
 """
 ForML provider configs.
 """
-import abc
 import types
-
 import typing
 
 from forml import conf
 from forml.conf import section as secmod
 
 
-class Section(metaclass=secmod.Meta):
-    """Provider config container.
+class Section(secmod.Parsed):
+    """Special sections of forml providers config options.
     """
-    PATTERN = r'\s*(\w+)\s*$'
-    FIELDS = 'name, kwargs'
-    SELECTOR: str = conf.SECTION_DEFAULT
-    SUBJECT: str = abc.abstractmethod
-
-    def __new__(cls, ref: str):
-        section = f'{cls.SUBJECT.upper()}:{ref}'  # pylint: disable=no-member
-        secmod.ensure(conf.PARSER, section)
-        kwargs = dict()
-        for option, value in conf.PARSER.items(section):
-            if conf.PARSER.remove_option(section, option):  # take only non-default options
-                conf.PARSER.set(section, option, value)
-                kwargs[option] = value
-        provider = kwargs.pop(conf.OPT_PROVIDER, ref)
-        return super().__new__(cls, provider, types.MappingProxyType(kwargs))
+    FIELDS: typing.Tuple[str] = ('name', 'kwargs')
+    REFEREE: str = conf.SECTION_PLATFORM
 
     @classmethod
-    def _default(cls) -> 'Section':
-        """Default parsing.
-        """
-        return cls.parse(conf.get(cls.SUBJECT, cls.SELECTOR))
-
-    # pylint: disable=no-member
-    @classmethod
-    def parse(cls, ref: typing.Optional[str] = None) -> 'Section':
-        """Resolve the provider config.
-
-        Args:
-            ref: Provider alias.
-
-        Returns: Provider config instance.
-        """
-        return super().parse(ref)[0] if ref else cls.default
+    def extract(cls, reference: str, *_) -> typing.Tuple[typing.Any]:
+        kwargs = dict(super().extract(reference)[0])
+        provider = kwargs.pop(conf.OPT_PROVIDER, reference)
+        return provider, types.MappingProxyType(kwargs)
 
     def __hash__(self):
-        return hash(self.name)
+        return hash(self.name)  # pylint: disable=no-member
 
     def __eq__(self, other):
-        return isinstance(other, self.__class__) and other.name == self.name
+        return isinstance(other, self.__class__) and other.name == self.name  # pylint: disable=no-member
 
 
-class Registry(Section):
+class Registry(secmod.Single, Section):
     """Registry provider.
     """
-    SUBJECT = conf.OPT_REGISTRY
+    SELECTOR = conf.OPT_REGISTRY
 
 
-class Feed(Section):
-    """Feed provider.
+class Feed(secmod.Single, Section):
+    """Feed providers.
     """
-    SUBJECT = conf.OPT_FEED
+    SELECTOR = conf.OPT_FEED
 
 
-class Runner(Section):
+class Runner(secmod.Single, Section):
     """Runner provider.
     """
-    SUBJECT = conf.OPT_RUNNER
+    SELECTOR = conf.OPT_RUNNER
 
 
 class Testing:
     """Providers specific to testing.
     """
-    class Runner(Section):
+    class Runner(secmod.Single, Section):
         """Runner provider.
         """
-        SUBJECT = conf.OPT_RUNNER
-        SELECTOR = conf.SECTION_TESTING
+        REFEREE = conf.SECTION_TESTING
+        SELECTOR = conf.OPT_RUNNER
--- tests/conf/test_section.py
+++ tests/conf/test_section.py
@@ -19,7 +19,9 @@
 ForML section config unit tests.
 """
 # pylint: disable=protected-access,no-self-use
+import re
 import types
+import typing
 
 import pytest
 
@@ -27,38 +29,46 @@ from forml import error
 from forml.conf import section as secmod  # pylint: disable=unused-import
 
 
-class TestMeta:
+class Parsed(secmod.Parsed):
+    """Base class for parsed fixtures.
+    """
+    REFEREE = 'FOOBAR'
+
+
+class TestParsed:
     """SectionMeta unit tests.
     """
     def test_simple(self, conf: types.ModuleType):  # pylint: disable=unused-argument
         """Test with default pattern.
         """
-        class Simple(metaclass=secmod.Meta):  # pylint: disable=undefined-variable
+        class Simple(Parsed):
             """CSV specified field list.
             """
-            FIELDS = 'foo'
+            SELECTOR = 'foo'
 
         # pylint: disable=no-member
-        assert Simple.parse('') == ()
-        assert Simple.parse('bar')[0].foo == 'bar'
-        assert [s.foo for s in Simple.parse('bar, baz')] == ['bar', 'baz']
+        assert Simple.parse('bar')[0].kwargs['bar'] == 'foo'
         with pytest.raises(error.Unexpected):
             Simple.parse(',bar')
         with pytest.raises(error.Unexpected):
             Simple.parse('bar, bar')
 
     def test_complex(self, conf: types.ModuleType):  # pylint: disable=unused-argument
-        """Test for the secmod.Meta metaclass with default pattern.
+        """Test with complex patter, custom fields and extract method.
         """
-        class Complex(metaclass=secmod.Meta):  # pylint: disable=undefined-variable
+        class Complex(Parsed):
             """Complex specified field list.
             """
-            PATTERN = r'\s*(\w+)(?:\[(.*?)\])?\s*(?:,|$)'
-            FIELDS = 'foo, bar'
+            PATTERN = re.compile(r'\s*(\w+)(?:\[(.*?)\])?\s*(?:,|$)')
+            FIELDS = 'foo', 'kwargs'
+            SELECTOR = 'bar'
+
+            @classmethod
+            def extract(cls, reference: str, arg, *_) -> typing.Tuple[typing.Any]:
+                return arg, *super().extract(reference)
 
         # pylint: disable=no-member
-        assert Complex.parse('baz')[0] == ('baz', None)
-        assert [(s.foo, s.bar) for s in Complex.parse('baz[a, b]')] == [('baz', 'a, b')]
-        assert Complex.parse('baz[a, b], boo[c]') == (('baz', 'a, b'), ('boo', 'c'))
+        assert Complex.parse('bar') == ((None, {'bar': 'bar'}), )
+        assert Complex.parse('foo[bar, baz], bar') == (('bar, baz', {'foo': 'bar'}), (None, {'bar': 'bar'}))
         with pytest.raises(error.Unexpected):
             Complex.parse(',baz[a, b]')
