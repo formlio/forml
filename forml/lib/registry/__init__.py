fd070da74a0107e37c0c643dd8df8680618fef74
Antony Mayi <antonymayi@yahoo.com>
2021-10-13 23:15:21 +0000

    lib.registry default import path

--- forml/lib/registry/__init__.py
+++ forml/lib/registry/__init__.py
@@ -14,9 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-"""
-Registry implementations.
-"""
-
-__all__ = ['filesystem']
